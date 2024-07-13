2d9159144ec12ec5bd95c46fd36afb4eef0407cc
Daniel Felipe Maya Gómez <95893333+danielmaya2420@users.noreply.github.com>
2024-07-12 23:41:34 -0500

    Add files via upload

---
+++ alura_series.sql
@@ -0,0 +1,3 @@
+SELECT * FROM series WHERE series.total_temporadas <= 6 AND series.evaluacion >= 7.5
+SELECT * FROM episodios
+SELECT * FROM series
---
+++ src/test/java/com/aluracursos/ScreenmatchSpring/ScreenmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.ScreenmatchSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
